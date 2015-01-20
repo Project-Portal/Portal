eae81ee644d04065593f7355660b8f9f12967b26
Michael Rosenfield <mer95@case.edu>
2015-01-19 22:46:33 -0500

    Added run.js and began structure

--- Portal.js
+++ Portal.js
@@ -13,5 +13,23 @@ var PreviewModule = {
 		var iFrame = document.getElementById('portal');
 		iFrame.parentNode.removeChild(iFrame);
 		$("container").remove();
-	};
-}
+	},
+
+	buildFrame: function() {
+
+	},
+
+	buildButtons: function() {
+
+	},
+
+	//TAKE IN DIMENSIONS
+	resize: function() {
+
+	},
+
+	rotate: function() {
+
+	}
+
+};
