e75636e158a0858dc6839780e3eafca17b0262b6
Michael Rosenfield <mer95@case.edu>
2015-01-27 21:00:46 -0500

    Added Rotate method and enabled draggability

--- background.js
+++ background.js
@@ -34,7 +34,8 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
     ["blocking", "requestHeaders"]
 );
 */
-
+/*
+//****** OTHER WAY!!!! *********
   // The 'reqestFilter' parameter allows you to only listen for
   // certain requests. Chrome 17 requires that, at the very least,
   // it defines the URLs you wish to subscribe to. In the general
@@ -75,14 +76,16 @@ var requestFilter = {
     blockingResponse.requestHeaders = headers;
     return blockingResponse;
   };
+  */
  
-chrome.webRequest.onBeforeSendHeaders.addListener( handler, requestFilter, extraInfoSpec );
+//chrome.webRequest.onBeforeSendHeaders.addListener( handler, requestFilter, extraInfoSpec );
 
 function onMessage(request, sender, sendResponse) {
 	sendResponse({}); 
     chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
      activeTab = arrayOfTabs[0].url;
   });
+}
 
 
     
