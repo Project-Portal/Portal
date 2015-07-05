298c9e3aeb85ed34542ce4ef7b27a65ca0e1dda5
Michael Rosenfield <mer95@case.edu>
2015-07-04 18:24:46 -0700

    Added an error when setting portal key

--- options.js
+++ options.js
@@ -1,25 +1,46 @@
+///
 //saves the user's preferences
 function saveOptions() {
 	var hotkey = document.getElementById('hotkey').value.toLowerCase();
 	var dragResize = document.getElementById('dragResize').checked;
 	var defaultHotkey = document.getElementById('defaultHotkey').checked;
 
-	chrome.storage.sync.set({
-    "hotkey": hotkey,
-    'dragResize': dragResize,
-    'defaultHotkey': defaultHotkey
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-		status.className = "alert alert-success";
-		status.role = "alert";
-    status.textContent = 'Options saved!' + ' New Hotkey: ' + hotkey + '. Refresh the page to see your changes take effect.';
-    setTimeout(function() {
-      status.textContent = '';
-			status.role = '';
-			status.className = '';
-    }, 6000);
-  });
+	//If the hotkey inputed is a single character, set it.
+	if(hotkey.length == 1){
+		chrome.storage.sync.set({
+	    "hotkey": hotkey,
+	    'dragResize': dragResize,
+	    'defaultHotkey': defaultHotkey
+	  }, function() {
+	    // Update status to let user know options were saved.
+	    var status = document.getElementById('status');
+			status.className = "alert alert-success";
+			status.role = "alert";
+	    status.textContent = 'SUCCESS!' + ' New Hotkey: ' + hotkey + '. Refresh the page to see your changes take effect.';
+	    setTimeout(function() {
+	      status.textContent = '';
+				status.role = '';
+				status.className = '';
+	    }, 6000);
+	  });
+	}
+	//If the hotkey is >1 characters, throw an error.
+	else{
+		chrome.storage.sync.set({
+			//NOTHING
+		}, function() {
+			// Update status to let user know there was an error!
+			var status = document.getElementById('status');
+			status.className = "alert alert-danger";
+			status.role = "alert";
+			status.textContent = 'ERROR: Looks like the portal key is a word... Try picking a single key on your keyboard.';
+			setTimeout(function() {
+				status.textContent = '';
+				status.role = '';
+				status.className = '';
+			}, 6000);
+		});
+	}
 }
 
 function restoreOptions() {
